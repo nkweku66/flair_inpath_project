e0b755eeae39af6a4274ae2651e492da30d6fb51
nkweku66 <nkweku66@gmail.com>
2024-07-25 23:14:54 +0000

    Change file paths for images

--- src/assets/dataSource/data.ts
+++ src/assets/dataSource/data.ts
@@ -1,7 +1,7 @@
 export default [
     {
         index: 1,
-        imageSrc: "./src/assets/images/card_img1.png",
+        imageSrc: "../images/card_img1.png",
         imageWidth: "76px",
         cardTitle: "Candidate",
         cardInfo: "A graduate looking for an employment opportunity",
@@ -9,7 +9,7 @@ export default [
     },
     {
         index: 2,
-        imageSrc:"./src/assets/images/card_img2.png",
+        imageSrc:"../images/card_img2.png",
         imageWidth: "73.29px",
         cardTitle:"Employer",
         cardInfo:"An organization looking to hire top talents",
@@ -17,7 +17,7 @@ export default [
     },
     {
         index: 3,
-        imageSrc:"./src/assets/images/card_img3.png",
+        imageSrc:"../images/card_img3.png",
         imageWidth: "60.66px",
         cardTitle:"Mentor",
         cardInfo:"A professional helping others navigate their careers",
@@ -26,7 +26,7 @@ export default [
     },
     {
         index: 4,
-        imageSrc:"./src/assets/images/card_img4.png",
+        imageSrc:"../images/card_img4.png",
         imageWidth: "97.11px",
         cardTitle:"Trainer",
         cardInfo:"An Organization offering training programmes",
@@ -59,38 +59,38 @@ export const profileDetails = [
         "key": 1,
         "heading": "Education",
         "details": "School info, field of study",
-        "icon": "../src/assets/images/icon-1.png"
+        "icon": "../images/icon-1.png"
         
     },
     {
         "key": 2,
         "heading": "Work Experience",
         "details": "Where you've worked, your role",
-        "icon": "../src/assets/images/icon-2.png"
+        "icon": "../images/icon-2.png"
     },
     {
         "key": 3,
         "heading": "Interests& Skills",
         "details": "Career interests and skillset",
-        "icon": "../src/assets/images/icon-3.png"
+        "icon": "../images/icon-3.png"
     },
     {
         "key": 4,
         "heading": "Certifications",
         "details": "Certifications worth highlighting",
-        "icon": "../src/assets/images/icon-4.png"
+        "icon": "../images/icon-4.png"
     },
     {
         "key": 5,
         "heading": "Accomplishments",
         "details": "Personal achievements & impact",
-        "icon": "../src/assets/images/icon-5.png"
+        "icon": "../images/icon-5.png"
     },
     {
         "key": 6,
         "heading": "Upload Resume",
         "details": "CV, portfolio",
-        "icon": "../src/assets/images/icon-6.png"
+        "icon": "../images/icon-6.png"
     }
 ]
 
--- src/components/searchTool/SearchSection.tsx
+++ src/components/searchTool/SearchSection.tsx
@@ -3,13 +3,12 @@ import * as S from "./SearchSection.styles"
 const SearchSection = () => {
     return (
         <S.SearchContainer>
-            <S.SearchAreaImage src="./src/assets/images/chef.png" />
-            {/* <S.SearchAreaImage src="./src/assets/images/guy.png"  variant2 position/> */}
+            <S.SearchAreaImage src="../images/chef.png" />
             <S.SearchHeadingsWrapper>
                 <S.SearchHeader>Discover a greater you.</S.SearchHeader>
                 <S.SearchSubHeader>We help you <S.Highlight>find the perfect job</S.Highlight></S.SearchSubHeader>
             </S.SearchHeadingsWrapper>
-            <S.SearchAreaImage src="./src/assets/images/uniguy.png" variant1/>
+            <S.SearchAreaImage src="../images/uniguy.png" variant1/>
             <S.SearchArea>
                 <S.SearchInputWrapper>
                     <S.SearchInput placeholder='Try "Civil Engineer"'/>
@@ -21,7 +20,7 @@ const SearchSection = () => {
                 </S.SearchInputWrapper>
                 <S.SearchButton><S.SearchArrowIcon></S.SearchArrowIcon></S.SearchButton>
             </S.SearchArea>
-            <S.SearchAreaImage src="./src/assets/images/guy.png" variant2/>
+            <S.SearchAreaImage src="../images/guy.png" variant2/>
         </S.SearchContainer>
     )
 }
