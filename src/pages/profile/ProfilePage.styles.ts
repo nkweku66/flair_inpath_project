721549feefb4da1ff5d3f863856c982e5cf43f26
nkweku66 <nkweku66@gmail.com>
2024-07-26 12:14:38 +0000

    Responsivesness for tablet view

--- src/pages/profile/ProfilePage.styles.ts
+++ src/pages/profile/ProfilePage.styles.ts
@@ -2,6 +2,9 @@ import styled from 'styled-components'
 
 interface Props {
     variant?: boolean;
+    variant2?: boolean;
+    selected?: boolean;
+    openClose?: boolean;
 }
 const Wrapper = styled.div`
     padding: 1.5em;
@@ -9,6 +12,22 @@ const Wrapper = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    width: 100%;
+
+    @media screen and (min-width: 744px) {
+        padding: 48px 2.5em;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: flex-start;
+        gap: 1.5em;
+        border: 1px solid red;
+    }
+
+    @media screen and (min-width: 1440px){
+        padding: 48px 376px 0 104px; 
+        justif-content: center;
+        gap: 5em;
+    }
 `
 const ProfileCards = styled.div`
     display: flex;
@@ -17,20 +36,33 @@ const ProfileCards = styled.div`
     align-items: center;
     width: 100%;
     height: 100%;
-
 `
-const CardLines = styled.span`
-    height: 2px;
-    width: 12px;
-    background-color: #000;
+
+const AddAccordion = styled.div`
+    // border: 1px solid red;
+    width: 20px;
+    height: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     position: absolute;
-    left: 90%;
-    transition: transform .3s ease-in-out;
+    left: 80%;
+    z-index: 99;
+    cursor: pointer;
+
+`
+const CardLines = styled.span<Props>`
+    width: 2px;
+    height: 15px;
+    // margin: 0 auto;
+    background-color: #000;
+    cursor: pointer;
+    transition: transform .3s ease-in-out, scale .3s ease-in-out;
+    transform: ${(props) => props.selected? "scale(0)" : "scale(1)"};
 `
 
-const CardVerticalLine = styled(CardLines)`
-    transform: rotate(90deg);
-    
+const CardVerticalLine = styled(CardLines)<Props>`
+    transform: ${(props) => props.selected? "translate3d(0, 0, 0) rotate(270deg)" : "translate3d(-1px, 0, 0) rotate(90deg)"};
 `
 
 const ProfileCard = styled.section`
@@ -56,7 +88,7 @@ const ProfileCard = styled.section`
 `
 
 const ProfileGreenCard = styled.section`
-    width: 100%;
+    width: 205px;
     height: 88px;
     padding: 2px;
     display: flex;
@@ -66,7 +98,53 @@ const ProfileGreenCard = styled.section`
     padding: 20px 16px;
     margin: 1.5em 0;
     font-weight: 600;
-    background: url('./src/assets/images/greenbackground.png') center center / cover;
+    background: url('../images/greenbackground.png');
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    background-position: center;
+    
+    position: relative;
+
+    @media screen and (min-width: 744px) {
+        margin: 0;
+
+        &::after {
+            content: "";
+            background: #0b8659;
+            width: 100%;
+            height: 336px;
+            position: absolute;
+            top: 100%;
+            left: 0;
+        }
+    }
+
+    @media screen and (min-width: 1440px){
+        width: 272px;
+        gap: 3em;
+    }
+`
+
+const ProfileAsideDetails = styled.aside`
+    display: none;
+    @media screen and (min-width: 744px) {
+        position: absolute;
+        top: 100%;
+        
+        z-index: 2;
+        margin-top: 1rem;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+        gap: 2em;
+        width: 100%;
+        height: 100%;
+    }
+
+    @media screen and (min-width: 1440px) {
+        // margin-top: 2rem;
+    }
 `
 
 const ProfileCardHeader = styled.header`
@@ -88,8 +166,13 @@ const ProfileText = styled.p`
     color: #818181;
 `
 
-const ProfileHeading = styled.h3`
-    font-size: 1rem;
+const ProfileHeading = styled.h3<Props>`
+    font-size: ${(props) => {
+        if (props.variant) return "1.2rem";
+        else if (props.variant2) return "0.8rem"
+        else return "1rem" 
+        }
+    };
     font-weight: 600;
     // margin-bottom: 2em;
 `
@@ -105,6 +188,13 @@ const ProfileBtnContainer = styled.nav`
     margin-top: 52px;
     // border: 1px solid red;
     border-top: 1px solid rgba(0, 0, 0, 0.1);
+
+    @media screen and (min-width: 744px) {
+        margin-left: 0;
+        margin-top: 0;
+        gap: 1em;
+    }
+)
 `
 const ProfileBtn = styled.button<Props>`
     width: 152px;
@@ -115,12 +205,32 @@ const ProfileBtn = styled.button<Props>`
     color: ${(props) => props.variant? "#818181" : "#000"};
     font-family: inherit;
     font-weight: 600;
+    cursor: pointer;
+    transition: background .3s ease-in-out, color .3s ease-in-out;
+
+    &:hover {
+        background-color: #0B8659;
+        color: #fff;
+    }
+
+    @media screen and (min-width: 744px) {
+        width: 100%;
+    }
 `
 const ProfileCardIcon = styled.img`
     width: 20px;
     height: 20px;
     object-fit: contain;
     margin-right: 1em;
+`
+
+const ProfileForm = styled.div`
+    width: 100%;
+    position: relative;
+
+    @media screen and (min-width: 1440px) {
+        width: 608px;
+    }
 `
 
 export {
@@ -135,5 +245,8 @@ export {
     ProfileCardHeader,
     CardLines,
     CardVerticalLine,
-    ProfileGreenCard
+    ProfileGreenCard,
+    ProfileForm,
+    ProfileAsideDetails,
+    AddAccordion
 }
--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -2,8 +2,17 @@ import * as S from "./ProfilePage.styles"
 import Logo from "../../components/atomicComponenets/Logo"
 import { profileDetails } from "../../assets/dataSource/data"
 import ProgressCircle from "../../components/atomicComponenets/ProgressCircle"
+import { useState } from "react"
+
 
 const ProfilePage = () => {
+    const [selectedIndex, setselectedIndex] = useState<number | null>(null);
+
+    const handleClicks = (index: number) => {
+        setselectedIndex(prevIndex => (prevIndex == index ? null : index));
+
+    };
+
 
     const ProfileCard = profileDetails.map((profile) => {
         return (
@@ -11,8 +20,12 @@ const ProfilePage = () => {
                     <S.ProfileCardHeader>
                         <S.ProfileCardIcon src={profile.icon}/>
                         <S.ProfileHeading>{profile.heading}</S.ProfileHeading>
-                        <S.CardLines></S.CardLines>
-                        <S.CardVerticalLine></S.CardVerticalLine>
+                        <S.AddAccordion
+                         onClick={() => handleClicks(profile.key)}
+                        >
+                            <S.CardLines selected={selectedIndex === profile.key} openClose={selectedIndex === profile.key}></S.CardLines>
+                            <S.CardVerticalLine selected={selectedIndex === profile.key}></S.CardVerticalLine>
+                        </S.AddAccordion>
                     </S.ProfileCardHeader>
                     <S.ProfileText>{profile.details}</S.ProfileText>
                 </S.ProfileCard>
@@ -25,16 +38,30 @@ const ProfilePage = () => {
                 // children={<S.BackArrow></S.BackArrow>}
             />
             <S.Wrapper>
-            <S.ProfileHeading>Set up your profile.</S.ProfileHeading>
                 <S.ProfileGreenCard>
-                    Profile Completion
-                    <ProgressCircle percentage={0} size={60} strokeWidth={7} />
+                        Profile Completion
+                        <ProgressCircle percentage={0} size={60} strokeWidth={7} />
+                        <S.ProfileAsideDetails>
+                            <S.ProfileHeading variant2>Education</S.ProfileHeading>
+                            <S.ProfileHeading variant2>Work Experience</S.ProfileHeading>
+                            <S.ProfileHeading variant2>Interests & Skills</S.ProfileHeading>
+                            <S.ProfileHeading variant2>Certifications</S.ProfileHeading>
+                            <S.ProfileHeading variant2>Accomplishments</S.ProfileHeading>
+                            <S.ProfileHeading variant2>Upload Resume</S.ProfileHeading>
+                        </S.ProfileAsideDetails>
                 </S.ProfileGreenCard>
-                {ProfileCard}
-                <S.ProfileBtnContainer>
-                    <S.ProfileBtn>i'll do it later</S.ProfileBtn>
-                    <S.ProfileBtn variant>Done</S.ProfileBtn>
-                </S.ProfileBtnContainer>
+                <S.ProfileForm>
+                    <S.ProfileHeading variant>Set up your profile.</S.ProfileHeading>
+                    {/* <S.ProfileGreenCard>
+                        Profile Completion
+                        <ProgressCircle percentage={0} size={60} strokeWidth={7} />
+                    </S.ProfileGreenCard> */}
+                    {ProfileCard}
+                    <S.ProfileBtnContainer>
+                        <S.ProfileBtn>i'll do it later</S.ProfileBtn>
+                        <S.ProfileBtn variant>Done</S.ProfileBtn>
+                    </S.ProfileBtnContainer>
+                </S.ProfileForm>
             </S.Wrapper>    
         </>
     )
